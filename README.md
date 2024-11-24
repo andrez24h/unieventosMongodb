20dd002a7ad21818e7ca54dad83559874b8aa0cb
andres24h <andres24@hotmail.com>
2024-11-24 17:21:41 -0500

    Fase 1

---
+++ README.md
@@ -0,0 +1,7 @@
+## Multilocal
+
+--> Proyecto que busca establecer una plataforma virtual.
+
+--> Es un proyecto en lenguaje Java contruido por Gradle.
+
+--> Es un proyecto experimental.
---
+++ src/main/java/dev/andresm/MultilocalApplication.java
@@ -0,0 +1,13 @@
+package dev.andresm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultilocalApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MultilocalApplication.class, args);
+    }
+}
